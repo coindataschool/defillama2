fc4cbb7ee0571713590f81893adfcbc4d1d13323
Coin Data School <coindataschool@gmail.com>
2022-10-05 13:04:42 +0900

    update setup.py

--- setup.py
+++ setup.py
@@ -2,17 +2,22 @@
 
 from setuptools import setup
 
-VERSION = '1.0'
+VERSION = '0.5'
 
 packages = ['defillama2']
-requires = ['requests>=2.28.1', 'pandas>=1.4.4', 'numpy>=1.22.4', 'dateutil>=2.8.2']
+requires = ['requests>=2.28.1', 'pandas>=1.4.4', 'numpy>=1.22.4', 
+    'python-dateutil>=2.8.2']
+
+with open('README.md', mode='r') as f:
+    readme = f.read()
 
 setup(
     name="defillama2", 
     version=VERSION,
-    description='Python client for DefiLlama APIs',
-    long_description='Download and clean data from DefiLlama.com.',
-    url='https://github.com/coindataschool/defillama',
+    description='Python client for DefiLlama API',
+    long_description=readme,
+    long_description_content_type='text/markdown',
+    url='https://github.com/coindataschool/defillama2',
     author="Coin Data School",
     author_email="<coindataschool@gmail.com>",
     packages=packages,
